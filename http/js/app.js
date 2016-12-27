dc7effd2d045d3608d14310554cc7264f73b4b54
Steven Luong <ste.luong@gmail.com>
2016-12-27 15:39:33 +0100

    login signup logout angular loopback

--- http/js/app.js
+++ http/js/app.js
@@ -3,21 +3,49 @@
 /* App Module */
 
 var mainApp = angular.module('mainApp', [
-  'ngRoute',
-  //'mainAnimations',
-  'mainControllers',
-  'mainFilters',
-  'mainServices'
-]);
+        'ngRoute',
+        //'mainAnimations',
+        'mainControllers',
+        'mainFilters',
+        'lbServices',
+        //'satellizer',
+        'mainServices'
+])
+/*
+.config(function($authProvider) {
+});
 
+*/
+.config(function(LoopBackResourceProvider) {
+
+    // Use a custom auth header instead of the default 'Authorization'
+    //      LoopBackResourceProvider.setAuthHeader('X-Access-Token');
+    //
+    //         // Change the URL where to access the LoopBack REST API server
+    LoopBackResourceProvider.setUrlBase('http://slapps.fr:3000/api');
+});
 mainApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/main.html',
-        controller: 'mainCtrl'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
+        function($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/main.html',
+                    controller: 'mainCtrl'
+                }).
+            when('/login', {
+                templateUrl: 'partials/login.html',
+                controller: 'loginCtrl'
+            }).
+             when('/signup', {
+                templateUrl: 'partials/signup.html',
+                controller: 'signupCtrl'
+            }).
+             when('/logout', {
+                templateUrl: 'partials/logout.html',
+                controller: 'logoutCtrl'
+            }).
+
+
+            otherwise({
+                redirectTo: '/'
+            });
+        }]);
--- http/js/controllers.js
+++ http/js/controllers.js
@@ -3,9 +3,53 @@
 /* Controllers */
 
 var mainControllers = angular.module('mainControllers', []);
-mainControllers.controller('mainCtrl', ['$scope','Sources',
-        function($scope, Sources) {
-            //Sources.getSources().success(function(response){
-            //    console.log(response);
-            //});
+mainControllers.controller('mainCtrl', ['$scope','Sources','User','$location',
+        function($scope, Sources,User,$location) {
+            $scope.user ={};
+            $scope.user.isAuthenticated = User.isAuthenticated();
+            $scope.user.id = User.getCurrentId();
+        }]);
+mainControllers.controller('loginCtrl', ['$scope','User','$location',
+        function($scope,User,$location) {
+            $scope.submit = function(){
+                var credentials = {
+                    email:$scope.email,
+                    password:$scope.password
+                };
+                $scope.user = User.login(credentials, function(){
+                    console.log($scope.user.id);
+                    $location.path("/");
+                },function(res){
+                    console.log(res);
+                });
+
+            }
+        }]);
+mainControllers.controller('logoutCtrl', ['$scope','User','$location',
+        function($scope,User,$location) {
+            User.logout(function(){
+                    console.log("OK");
+                    $location.path("/");
+                },function(res){
+                    console.log(res);
+                });
+
+            }
+        ]);
+mainControllers.controller('signupCtrl', ['$scope','User','$location',
+        function($scope,User,$location) {
+            $scope.submit = function(){
+                var credentials = {
+                    email:$scope.email,
+                    password:$scope.password
+                };
+                $scope.user = User.create(credentials, function(){
+                    console.log($scope.user.id);
+                    console.log("OK");
+                    $location.path("/");
+                },function(res){
+                    console.log(res);
+                });
+
+            }
         }]);
